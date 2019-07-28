aca6e2049afbc34003a16db8889cd200bb8a0d65
Greg Sparks <gsparks@westmont.edu>
2019-07-28 16:35:31 -0700

    Added seed.rb file to load up spreadsheet data

--- shipstr/db/seeds.rb
+++ shipstr/db/seeds.rb
@@ -1,7 +1,22 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+CSV.foreach(Rails.root.join('db/seeds_data/service_provider_data.csv'), headers: true) do |row|
+    ShippingServiceProvider.create({
+    id: row[0],
+    company_name: row[1],
+    rate_cents: (row[2].to_f * 100).to_i,
+    currency: row[3]
+  })
+end
+
+CSV.foreach(Rails.root.join('db/seeds_data/rate_data.csv'), headers: true) do |row|
+    ShippingRate.create({
+    rate_cents: (row[0].to_f * 100).to_i,
+    currency: row[1],
+    origin: row[2],
+    destination: row[3],
+    shipping_service_provider_id: row[4]
+  })
+end
